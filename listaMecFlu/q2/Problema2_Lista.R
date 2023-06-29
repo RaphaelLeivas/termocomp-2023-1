2aa862eb4586add97b1f6296bcbace0e772848f1
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2023-06-29 20:24:55 -0300

    WIP questao 2, com resultado incorreto

---
+++ listaMecFlu/q2/Problema2_Lista.R
@@ -0,0 +1,67 @@
+rm(list = ls())
+
+library(flextable)
+
+dpdz <- -12.928
+mu <- 1.01 * 10^-3
+R <- 25 * 10^-3
+N <- 10 # numero de pontos analisados 
+dr <- R/N # passo no espaço
+
+N <- N + 1
+
+r_seq <- seq(from = 0.0, to = R, by = dr)
+u_analytical <- dpdz / (4 * mu) * (r_seq^2 - R^2)
+
+plot(
+  NULL,
+  main = "Velocidade (m/s) x Posição radial (mm)",
+  xlab = "Posição radial (mm)",
+  ylab = "Velocidade (m/s)",
+  xlim = c(0, max(r_seq)),
+  ylim = c(0, 5 * max(u_analytical))
+)
+
+lines(r_seq, u_analytical, lwd = 2, col = "red")
+
+# agora é a analise numerica
+
+# matriz que representa os coeficientes do sistema linear
+A <- matrix(0, ncol = N, nrow = N)
+line <- 0
+
+# matriz coluna (vetor vertical) que representa os termos independentes
+b <- rep(0, N)
+
+for (ri in r_seq) {
+  line <- line + 1
+  
+  if (ri == 0) {
+    # condição de simetria
+    A[line, line + 1] <- 1 + ri
+    A[line, line] <- -1 - ri
+    
+    b[line] <- (dpdz / mu) * ri * dr
+  } else if (ri == R) {
+    # na borda do cano, velocidade nula (cond de contorno)
+    A[line, line] <- -1 - ri
+    A[line, line - 1] <- ri
+    
+    b[line] <- (dpdz / mu) * ri * dr
+  } else {
+    # no interno
+    A[line, line + 1] <- 1 + ri
+    A[line, line] <- -1 - ri
+    A[line, line - 1] <- ri
+    
+    b[line] <- (dpdz / mu) * ri * dr
+  }
+}
+
+u_numerical <- solve(A) %*% b
+lines(r_seq, u_numerical, lwd = 2, col = "blue")
+
+
+
+
+
